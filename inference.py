94dd44d3248f11b7dfd26f97ad51198378420c61
Prabhat Sharma <hi.prabhat@gmail.com>
2020-05-24 05:15:30 -0700

    python inference is working. Also persisting scaler. Need to reuse scaler.

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import boto3
 import numpy as np
+import json
 
 
 client = boto3.client('sagemaker-runtime')
@@ -8,29 +9,28 @@ mean =  np.array([498.907, 50.0303, 29.4696, 2.289492000000002, 74.4993700000002
 std =  np.array([289.87433115711855, 28.657887384818498, 11.498821597442676, 1.306044393246921, 526.7579254124425])
 
 inference_data = [758,64,36,2.83,50.28]
+
+# numpy array to bytes
 test_a1 = np.array(inference_data).reshape(1,5)
 test_a1 = (inference_data - mean)/std
 test_a1 = np.float32(test_a1)
+test_a1 = test_a1.tolist()
+test_a1 = ",".join(map(str, test_a1))
 
-print(test_a1.tolist())
+print(test_a1)
 
-request = {
-  "instances": [
-    {
-      "data": test_a1.tolist()
-    }
-  ]
-}
+d1 = '0.89381146,  0.4874644,   0.5679191,   0.41385117, -0.04597818'
+
 
 response = client.invoke_endpoint(
     EndpointName='kmeans-2020-05-16-03-23-14-106',
-    Body=request,
-    ContentType='text/json',
-    Accept='string',
-    # TargetModel='string'
+    Body = test_a1,
+    ContentType='text/csv',
 )
 
-print(response)
+result = json.loads(response['Body'].read().decode())
+
+print(result)
 
 
 
--- laptop/kmeans.py
+++ laptop/kmeans.py
@@ -2,15 +2,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+import joblib
 
 data = pd.read_csv('diabetes_data.csv')
 
-print(data.head())
+scaler = MinMaxScaler()
+
+scaler.fit(data)
+
+joblib.dump(scaler, "scaler.gz")
+
+
+# scaler = joblib.load("scaler.gz")
+
+print(scaler.feature_range)
+
+t_data = scaler.transform(data)
+
+
+# print(data.head())
 
 Kmean = KMeans(n_clusters=5)
-result = Kmean.fit(data)
+result = Kmean.fit(t_data)
 
 X = [590, 91, 45, 2.39, 8.06]
 X = np.reshape(X, (1,-1))
-print(result.predict(X))
+t_X = scaler.transform(X)
+print(result.predict(t_X))
+
 
